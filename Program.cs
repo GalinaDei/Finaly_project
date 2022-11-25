f70be338753391a12e940eff84baf9f633f57de0
Galina <gal-dej@yandex.ru>
2022-11-25 13:54:52 +0300

    A bit testing

--- Program.cs
+++ Program.cs
@@ -2,14 +2,17 @@
 длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с 
 клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется 
 пользоваться коллекциями, лучше обойтись исключительно массивами. */
-string[] array= {"Name","Age","Adress","23","@"};
+string[] array= {"Name","UsersAge","Adress","2342","111@"};
 void PrintArray (string[]array)
 {
-  for (int i = 0; i < array.Length-1; i++)
+  if (array.Length>0)  
+  { for (int i = 0; i < array.Length-1; i++)
     {
       Console.Write($"{array[i]}, ");
     }
   Console.Write($"{array[array.Length-1]}");
+  }
+  else Console.Write("");
 }
 string[] CreateArrayFromSelectedElements (string[]array)
 {
